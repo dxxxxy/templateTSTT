44eb62cbb54c52631732c68580407dd60dbe366a
dxxxxy <introsdream@gmail.com>
2023-09-15 19:24:56 -0400

    fix: style greet message

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -10,11 +10,11 @@
     }
 </script>
 
-<div>
+<div class="flex flex-col items-center justify-center">
     <form class="flex gap-1" on:submit|preventDefault={greet}>
         <input class="px-4 py-2 font-semibold text-sm bg-gray-500 text-white rounded-l-lg shadow-sm" placeholder="dxxxxy was here" bind:value={name} />
         <button class="px-4 py-2 font-semibold text-sm bg-gray-500 text-white rounded-r-lg shadow-sm" type="submit">Greet</button>
     </form>
 
-    <p>{greetMsg}</p>
+    <p class="text-white text-sm font-semibold">{greetMsg}</p>
 </div>
