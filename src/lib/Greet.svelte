5809490ac4b25ca7768164e4eede61e110354497
dxxxxy <introsdream@gmail.com>
2023-09-15 19:14:40 -0400

    feat: play around with tailwind

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
-
-  let name = "";
-  let greetMsg = ""
-
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
-  }
+    import { invoke } from "@tauri-apps/api/tauri"
+    
+    let name = "";
+    let greetMsg = ""
+    
+    async function greet(){
+        //learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        greetMsg = await invoke("greet", { name })
+    }
 </script>
 
 <div>
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+    <form class="flex gap-1" on:submit|preventDefault={greet}>
+        <input class="px-4 py-2 font-semibold text-sm bg-gray-500 text-white rounded-l-lg shadow-sm" placeholder="dxxxxy was here" bind:value={name} />
+        <button class="px-4 py-2 font-semibold text-sm bg-gray-500 text-white rounded-r-lg shadow-sm" type="submit">Greet</button>
+    </form>
+
+    <p>{greetMsg}</p>
 </div>
